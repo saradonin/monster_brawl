ab7f8adc3be7e6eccaf963aa5eeac04ba655dc60
saradonin <m.jemieljanczuk@gmail.com>
2023-04-01 14:22:13 +0200

    dialogues moved to external file

--- brawl.py
+++ brawl.py
@@ -1,44 +1,5 @@
 import random
-
-# dialogues
-greetings = """Greetings adventurer! 
-Navigate through the game using buttons from [1] to [4] on your keyboard.
-"""
-
-fight_or_flight = """Fight or flight? 
-  1. [FIGHT] I'd love to see a good brawl!
-  2. [FLIGHT] I'd rather stay home and read a book.
-  """
-bye_answer = "Ok then, bye! Come back later."
-wrong_answer = "I knew you'd try it. Let's settle this in a civilized manner. Try again. \n"
-intro_bug = "It appears that this game is full of bugs!"
-intro_rat = "Squeak!"
-intro_goblin = "I don't have time for this..."
-intro_orc = "Victory or death! Aaaaarghh!"
-intro_stone_golem = "Flesh. Weak. Return to the earth."
-intro_the_elder_god = "All places, all things have souls. All souls can be devoured."
-
-choose_your_class = """Choose your class: 
-  1. Fighter
-  2. Rogue
-  3. Sorcerer
-  4. Warlock
-  """
-player_char = {
-    "name": ["Fighter", "Rogue", "Sorcerer", "Warlock"],
-    "hp": [16, 10, 8, 14],
-    "max_damage": [10, 4, 20, 2],
-    "attacks": [1, 3, 1, 4],
-}
-monsters = {
-    "name": ["Bug", "Rat", "Goblin",
-             "Orc", "Stone Golem", "The Elder God"],
-    "hp": [1, 4, 8, 16, 32, 1023],
-    "max_damage": [1, 1, 4, 8, 3, 255],
-    "attacks": [4, 2, 1, 1, 1, 1],
-    "intro": [intro_bug, intro_rat, intro_goblin,
-              intro_orc, intro_stone_golem, intro_the_elder_god]
-}
+from dialogues import greetings, fight_or_flight, choose_your_class, bye_answer, player_char, monsters
 
 
 def validate_input(message, num=2):
@@ -73,6 +34,7 @@ def choose_monster():
     monster_type = random.randint(0, monster_index)
     monster_name = monsters["name"][monster_type]
     monster_intro = monsters["intro"][monster_type]
+    print(f"You encountered {monster_name}: {monster_intro}")
     monster_attacks = monsters["attacks"][monster_type]
     monster_max_damage = monsters["max_damage"][monster_type]
     monster_hp = monsters["hp"][monster_type]
@@ -124,7 +86,7 @@ def fight():
 
             else:  # win and loose conditions
                 if monster_hp <= 0 and player_hp > 0:
-                    print("You won!", "\n")
+                    print("You won!")
                     win_counter += 1
                 elif player_hp <= 0 and monster_hp > 0:
                     print(f"You lost. {monster_name} won.")
