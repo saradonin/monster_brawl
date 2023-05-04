bd78b52ff55406024628170e2d8ed85b2bb4ad11
saradonin <m.jemieljanczuk@gmail.com>
2023-05-04 23:51:49 +0200

    player clas choice refactored monster choice reworked main fight reworked

--- brawl.py
+++ brawl.py
@@ -7,6 +7,7 @@ class Creature:
         self.max_hp = max_hp
         self.attacks_num = attacks_num
         self.max_damage = max_damage
+
         self.hp = max_hp
 
     def damage(self, enemy):
@@ -17,11 +18,17 @@ class Creature:
             damage_done += strike
         return damage_done
 
+    def set_hp(self, damage_taken):
+        self.hp = self.hp - damage_taken
+        if self.hp > 0:
+            return self.hp
+        else:
+            return f"{self.name} died!"
+
 
 class PlayerClass(Creature):
     def __init__(self, name, max_hp, attacks_num, max_damage):
         super().__init__(name, max_hp, attacks_num, max_damage)
-
 
 
 
@@ -39,13 +46,6 @@ mage = PlayerClass('Mage', 8, 1, 20)
 warlock = PlayerClass('Warlock', 14, 4, 2)
 player_char = [warrior, rogue, mage, warlock]
 
-choose_your_class = """Choose your class: 
-  1. Warrior
-  2. Rogue
-  3. Mage
-  4. Warlock
-  """
-
 # create monsters
 bug = Enemy('Bug', 1, 1, 4, 1, "It appears that this game is full of bugs!")
 rat = Enemy('Rat', 1, 4, 2, 1, "Squeak!")
@@ -55,11 +55,11 @@ owlbear = Enemy('Owlbear', 3, 20, 1, 6, "HOOT-GROWL!")
 stone_golem = Enemy('Stone Golem', 4, 32, 1, 3, "Flesh. Weak. Return to the earth.")
 froghemoth = Enemy('Froghemoth', 4, 60, 2, 4, "Aaaaaughibbrgubugbugrguburgle!")
 elder_god = Enemy('The Elder God', 5, 1023, 1, 255, "All places, all things have souls. All souls can be devoured.")
+monsters_list = [bug, rat, goblin, orc, owlbear, stone_golem, froghemoth, elder_god]
 
-
-print(Creature.damage(rogue, owlbear))
-print(Creature.damage(owlbear, warrior))
-
+# test
+# print(Creature.damage(elder_god, stone_golem))
+# print(Creature.set_hp(orc, 2))
 
 def validate_input(message, num=2):
     """
@@ -85,61 +85,35 @@ def validate_input(message, num=2):
 def choose_player_class():
     """
     Takes user input and returns stats of player class.
-    :return: tuple (int, int, int, int) - class stats
+    :return: tuple (str, int, int, int) - class stats
     """
-    message = choose_your_class
-    player_choice = validate_input(message, len(player_char))
-    player_object = player_char[int(player_choice - 1)]
+    choose_your_class = """Choose your class: 
+      1. Warrior
+      2. Rogue
+      3. Mage
+      4. Warlock
+      """
+    player_choice = validate_input(choose_your_class, len(player_char))
+    player = player_char[int(player_choice - 1)]
 
-    player_name = player_object.name
-    player_max_damage = player_object.max_damage
-    player_attacks = player_object.attacks_num
-    player_hp = player_object.max_hp
-    return player_name, player_max_damage, player_attacks, player_hp
+    # return player.name, player.max_hp, player.hp, player.attacks_num, player.max_damage
+    return player
 
-print(choose_player_class())
 
-def choose_monster(win_counter):
+def choose_enemy(win_counter=0):
     """
     Generates random monster form the list based on win counter, returns monster stats.
     :param win_counter: int - number of battles won
     :return: tuple (string, string, int, int, int) - monster stats
     """
+    monster = monsters_list[random.randint(0, len(monsters_list) - 1)]
+    # return monster.name, monster.intro, monster.max_hp, monster.hp, monster.attacks_num, monster.max_damage
+    return monster
 
-    monster_index = len(monsters["name"]) - 1  # dict range
-    if win_counter > 4:
-        monster_type = random.randint(0, monster_index)
-    elif win_counter > 2:
-        monster_type = random.randint(0, monster_index - 2)  # without 2 of the strongest
-    elif win_counter > 0:
-        monster_type = random.randint(0, monster_index - 4)  # without 4 of the strongest
-    else:
-        monster_type = random.randint(0, monster_index - 5)  # without 5 of the strongest
+# test
+# for i in range(0,10):
+#     print(i, choose_enemy())
 
-    monster_name = monsters["name"][monster_type]
-    monster_intro = monsters["intro"][monster_type]
-    print(f"You encountered {monster_name}: {monster_intro}")
-    monster_attacks = monsters["attacks"][monster_type]
-    monster_max_damage = monsters["max_damage"][monster_type]
-    monster_hp = monsters["hp"][monster_type]
-    return monster_name, monster_intro, monster_attacks, monster_max_damage, monster_hp
-
-
-def damage(name1, name2, max_damage, attacks):
-    """
-    Takes attacker stats and returns total amount of damage done by attacker.
-    :param name1: str - attacker name
-    :param name2: str - opponent name
-    :param max_damage: int - max damage value
-    :param attacks: int - number of attacks per turn
-    :return: int - total damage done
-    """
-    damage_done = 0
-    for i in range(attacks):
-        strike = random.randint(1, max_damage)
-        print(f"{name1} hit {name2} for {strike} points of damage")
-        damage_done += strike
-    return damage_done
 
 
 def end_message(win_counter):
@@ -161,10 +135,8 @@ def fight():
     This is where the fight happens.
     :return: str - end message
     """
-    print("""Greetings adventurer! 
-    Navigate through the game using buttons from [1] to [4] on your keyboard.
-    """)
-    player_index, player_max_damage, player_attacks, player_hp = choose_player_class()
+    print("Greetings adventurer! \nNavigate through the game using buttons from [1] to [4] on your keyboard.\n")
+    player = choose_player_class()
 
     win_counter = 0
     fight_or_flight = """
@@ -177,22 +149,23 @@ def fight():
         if decision == 2:
             return "Ok then, bye! Come back later."
         else:
-            monster_name, monster_intro, monster_attacks, monster_max_damage, monster_hp = choose_monster(win_counter)
+            enemy = choose_enemy(win_counter)
+            player.hp = player.max_hp
 
-            while player_hp > 0 and monster_hp > 0:
+            while player.hp > 0 and enemy.hp > 0:
                 # monster attack turn
-                monster_damage_done = damage(monster_name, "You", monster_max_damage, monster_attacks)
-                player_hp -= monster_damage_done
+                monster_damage_done = enemy.damage(player)
+                player.hp -= monster_damage_done
                 # player attack turn
-                player_damage_done = damage("You", monster_name, player_max_damage, player_attacks)
-                monster_hp -= player_damage_done
+                player_damage_done = player.damage(enemy)
+                enemy.hp -= player_damage_done
 
             else:  # win and loose conditions
-                if player_hp > 0 >= monster_hp:
+                if player.hp > 0 >= enemy.hp:
                     print("You won!")
                     win_counter += 1
-                elif monster_hp > 0 <= player_hp:
-                    print(f"You lost. {monster_name} won.")
+                elif enemy.hp > 0 <= player.hp:
+                    print(f"You lost. {enemy.name} won.")
                     return end_message(win_counter)
                 else:
                     print("You both died.")
@@ -201,4 +174,4 @@ def fight():
 # print(validate_input(message, 4))
 # print(choose_player_class())
 
-# print(fight())
+print(fight())
