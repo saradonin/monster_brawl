9586654f34314161e0e7fe81af36856e22427e76
saradonin <m.jemieljanczuk@gmail.com>
2023-05-06 22:05:45 +0200

    tests added

---
+++ test.py
@@ -0,0 +1,38 @@
+import brawl
+
+TEST_LEVEL_UP_FIGHTER = 0
+TEST_LEVEL_UP_ROGUE = 0
+TEST_LEVEL_UP_SORCERER = 0
+TEST_LEVEL_UP_WARLOCK = 0
+TEST_CHOOSE_ENEMY = True
+
+
+def test_level_up(creature, max_level):
+    result = f"{creature.name}: level {creature.level}, hp {creature.max_hp}, dmg {creature.max_damage}, " \
+             f"stat {creature.max_hp + creature.attacks_num * creature.max_damage} \n"
+    for i in range(-1, max_level):
+        creature.level_up()
+        result += f"{creature.name}: level {creature.level}, hp {creature.max_hp}, dmg {creature.max_damage}, " \
+                  f"stat {creature.max_hp + creature.attacks_num * creature.max_damage} \n"
+    return result
+
+
+def test_choose_enemy(level, number):
+    result = f"Enemies for player level {level} \n"
+    for i in range(number):
+        enemy = brawl.choose_enemy(level)
+        result += f"{enemy.name}: level {enemy.level} \n"
+    return result
+
+
+if TEST_LEVEL_UP_FIGHTER:
+    print(test_level_up(brawl.fighter, 9))
+if TEST_LEVEL_UP_ROGUE:
+    print(test_level_up(brawl.rogue, 9))
+if TEST_LEVEL_UP_SORCERER:
+    print(test_level_up(brawl.sorcerer, 9))
+if TEST_LEVEL_UP_WARLOCK:
+    print(test_level_up(brawl.warlock, 9))
+
+if TEST_CHOOSE_ENEMY:
+    print(test_choose_enemy(3, 10))
